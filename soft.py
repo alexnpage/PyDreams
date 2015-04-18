9a7e43e1e144668553f2da15fcaf813aa600c264
Alex Page <frigids@gmail.com>
2015-04-17 21:16:55 -0400

    Rewrite softlib code to be object-oriented and modular

--- soft.py
+++ soft.py
@@ -5,71 +5,115 @@ soft.py: Module for decompressing SoftLib archives
 """
 
 import sys
-import io
 import struct
-from collections import namedtuple
-
-SoftLibHdr = namedtuple('SoftLibHdr', 'version filecount')
-FileEntryHdr = namedtuple('FileEntryHdr', 'filename offset chunklen original_length compression')
-ChunkHeader = namedtuple('ChunkHeader', 'header_id original_length compression')
-
-def load_lib_shape(libname, filename):
-    pass
+from enum import Enum
 
 
-def load_lib_file(libname, filename):
-    file = open(libname, "rb")
+class Compression(Enum):
+    NONE = 0
+    LZW = 1
+    LZH = 2
 
-    # Verify it is a SLIB file
-    if file.read(4) != b'SLIB':
-        print(libname + " is not a SLIB file.")
-        file.close()
-        sys.exit(1)
 
-    # Check lib header version number
-    library_header = SoftLibHdr._make(struct.unpack('<hh', file.read(4)))
-    if library_header.version > 2:
-        print("Unsupported SLIB file version.")
-        file.close()
-        sys.exit(1)
+class SoftLibFile:
+    """
+    Represents a SoftLib archive and its contents.
 
-    # Manage file entry headers
-    file_entry_header = 0
-    file_found = False
-    for x in range(0, library_header.filecount):
-        file_entry_header = FileEntryHdr._make(struct.unpack('<16sIIIh', file.read(30)))
-        if file_entry_header.filename.decode('ascii').rstrip('\0') == filename:
-            file_found = True
-            break
+    The header formats are based upon commit 4348b47 of the Keen Dreams source.
+    https://github.com/keendreams/keen
+    """
 
-    chunk_header = 0
-    if file_found:
-        # Seek to file position
-        file.seek(file_entry_header.offset, io.SEEK_CUR)
+    class SoftLibHeader:
+        fmt = '<4sHH'
+        len = 8  # bytes
 
-        # Read chunk header and verify id
-        chunk_header = ChunkHeader._make(struct.unpack('<4shh', file.read(8)))
-        if chunk_header.header_id != b'CUNK':
-            print(filename + " - BAD header_id!")
-            file.close()
+        def __init__(self, header):
+            self.id = header[0].decode('ascii')  # 4-char string
+            self.version = header[1]  # unsigned short
+            self.file_count = header[2]  # unsigned short
+
+    class FileEntryHeader:
+        fmt = '<16sIIIh'
+        len = 30  # bytes
+
+        def __init__(self, header):
+            self.filename = header[0].decode('ascii').rstrip('\0')  # 16-char string, null-padded
+            self.offset = header[1]  # unsigned int
+            self.chunk_length = header[2]  # unsigned int
+            self.original_length = header[3]  # unsigned int
+            self.compression = Compression(header[4])  # short
+
+    class ChunkHeader:
+        fmt = '<4sIh'
+        len = 10  # bytes
+
+        def __init__(self, header):
+            self.id = header[0].decode('ascii')  # 4-char string
+            self.original_length = header[1]  # unsigned int
+            self.compression = Compression(header[2])  # short
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = open(filename, 'rb')
+
+        self.library_header, self.file_entry_headers, self.chunk_headers = self._read_lib()
+
+    def __del__(self):
+        self.file.close()
+
+    def __str__(self):
+        report = "Library:\n" \
+            "   ID: " + self.library_header.id + "\n" \
+            "   Version: " + str(self.library_header.version) + "\n" \
+            "   Files: " + str(self.library_header.file_count) + "\n\n"
+
+        for x, header in enumerate(self.file_entry_headers):
+            report += "File " + str(x+1) + ":\n" \
+                "   Filename: " + header.filename + "\n" \
+                "   Offset: " + format(header.offset, '#x') + "\n" \
+                "   Chunk Length: " + format(header.chunk_length, '#x') + "\n" \
+                "   Original Length: " + format(header.original_length, '#x') + "\n" \
+                "   Compression Method: " + str(header.compression).split('.')[1] + "\n\n"
+
+        for x, chunk in enumerate(self.chunk_headers):
+            report += "Chunk " + str(x+1) + ":\n" \
+                "   ID: " + chunk.id + "\n" \
+                "   Original Length: " + format(chunk.original_length, '#x') + "\n" \
+                "   Compression Method: " + str(chunk.compression).split('.')[1] + "\n\n"
+
+        return report
+
+    def _read_lib(self):
+
+        # Unpack library header
+        library_header = self.SoftLibHeader(struct.unpack(self.SoftLibHeader.fmt, self.file.read(self.SoftLibHeader.len)))
+
+        # Verify it is an SLIB file
+        if library_header.id != 'SLIB':
+            print("ERROR: " + self.filename + " is not a valid SLIB file.")
+            self.file.close()
             sys.exit(1)
 
-        # Calculate data length without header
-        chunk_len = file_entry_header.chunklen - 8
+        # Check lib header version number
+        if library_header.version > 2:
+            print("ERROR: Unsupported SLIB file version.")
+            self.file.close()
+            sys.exit(1)
 
-        # Extract file data
-        if chunk_header.compression == 1:  # LZW
-            pass
-        if chunk_header.compression == 2:  # LZH
-            pass
-        if chunk_header.compression == 0:  # None
-            pass
+        # Unpack file entry headers
+        file_entry_headers = []
+        for x in range(0, library_header.file_count):
+            file_entry_headers.append(
+                self.FileEntryHeader(struct.unpack(self.FileEntryHeader.fmt, self.file.read(self.FileEntryHeader.len)))
+            )
 
-    print(library_header)
-    print(file_entry_header)
-    print(chunk_header)
+        chunk_headers = []
+        header_size = self.SoftLibHeader.len + len(file_entry_headers) * self.FileEntryHeader.len
+        for header in file_entry_headers:
+            self.file.seek(header.offset + header_size)
+            chunk_headers.append(self.ChunkHeader(struct.unpack(self.ChunkHeader.fmt, self.file.read(self.ChunkHeader.len))))
 
-    file.close()
+        return library_header, file_entry_headers, chunk_headers
 
 if __name__ == '__main__':
-    load_lib_file("KDREAMS.CMP", "TITLESCR.LBM")
+    print(SoftLibFile(sys.argv[1]))
